858377f2ccbfacd7d37d82e008c26c4b19efa1b1
Zelal Buse Kizildag <zelalkizildag98@gmail.com>
2023-01-28 00:18:50 +0300

    @Named

---
+++ src/main/java/com/hamitmizrak/tutorials/JavaTutorials.java
@@ -0,0 +1,48 @@
+package com.hamitmizrak.tutorials;
+
+public class JavaTutorials {
+
+    public static void main(String[] args) {
+        //Arrays.sort();
+        //this. global değişkenleri çağırmak için, constructor bileşenleri çağırmak için kullanılır.
+        //super
+        //break_continue_return
+        //optional
+        //cast string to integer, boxing-unboxing, donusum yapilari
+        //primitive type
+        //wrapper class
+        //Random yapisi
+        //Math yapisi
+        //final - methodlarda kullanırsan override edilemez, classlarda extend edilemez
+        //static
+        //decoder encoder
+        //instanceof yapisi
+        //stringBuilder, string, stringTokenizer, stringbuffer
+        //Enum yapilari
+        //Multithread
+
+        //KISS, DRY, SOLID YAPILARI
+        //MONAD YAPISI
+        //List Set Map
+        //LinkedList ArrayList Hashmap Hashtable
+        //Generic <>
+        //Iterator
+        // UML
+        //POJO
+        //Bean
+        //Serileştirme
+        //IO
+        //JAR WAR
+        //Dağıtık
+        //constructor parametreli, parametresiz
+        //exception, error, fark
+        //java 8: stream, map, filter, sort
+        //optional yapısı
+        //lambda expression
+        //method referances
+        //
+
+
+
+    }
+}
