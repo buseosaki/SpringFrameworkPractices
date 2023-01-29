02e5fd206e2f5ce5d4b75f731e5cfe48c381d8e6
Zelal Buse Kizildag <zelalkizildag98@gmail.com>
2023-01-29 22:43:14 +0300

    @New

---
+++ src/main/java/com/hamitmizrak/StudentDto.java
@@ -0,0 +1,15 @@
+package com.hamitmizrak;
+
+import lombok.*;
+
+//Data Transfer Object = DTO
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class StudentDto {
+    private Long studentId;
+    private String studentName;
+    private String studentSurname;
+}
