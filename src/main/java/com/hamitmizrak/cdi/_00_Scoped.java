e7c237a48dafa0350eb70974716e5e824b199b61
Zelal Buse Kizildag <zelalkizildag98@gmail.com>
2023-01-31 20:02:10 +0300

    @Scoped info

---
+++ src/main/java/com/hamitmizrak/cdi/_00_Scoped.java
@@ -0,0 +1,17 @@
+package com.hamitmizrak.cdi;
+
+//kapsam. objenin yasam suresini belirler
+
+import javax.enterprise.context.*;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+//@ApplicationScoped //server kapatilana kadar calisir
+//@RequestScoped //1 istek boyunca
+//@SessionScoped 1 kullanici icin yasar logouta kadar
+//@Dependent //cagirilan yere gore olusur
+//@ConversationScoped //belli istek boyunca
+@SessionScoped //bean 1 instance olmasını saglamak icin
+public class _00_Scoped  {
+}
