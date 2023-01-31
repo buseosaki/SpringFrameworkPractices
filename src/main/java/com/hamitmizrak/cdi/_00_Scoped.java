08b23faaa60e5882639bfa4da0b9b09a553e7b81
Zelal Buse Kizildag <zelalkizildag98@gmail.com>
2023-01-31 20:47:48 +0300

    @Scoped info

--- src/main/java/com/hamitmizrak/cdi/_00_Scoped.java
+++ src/main/java/com/hamitmizrak/cdi/_00_Scoped.java
@@ -9,9 +9,10 @@ import java.io.Serializable;
 @Named
 //@ApplicationScoped //server kapatilana kadar calisir
 //@RequestScoped //1 istek boyunca
-//@SessionScoped 1 kullanici icin yasar logouta kadar
+@SessionScoped //1 kullanici icin yasar logouta kadar
 //@Dependent //cagirilan yere gore olusur
 //@ConversationScoped //belli istek boyunca
 @SessionScoped //bean 1 instance olmasını saglamak icin
 public class _00_Scoped  {
+
 }
